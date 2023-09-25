89206d27527672dcbe01d905cb9e3905a1d8f1c5
Bryce <bryce.h.bernstein@gmail.com>
2023-09-25 08:15:45 -0500

    Renamed docker container and made evaluator equal for Alice v. Alice and Alice v. Baab

--- Evaluation/evaluator.py
+++ Evaluation/evaluator.py
@@ -11,14 +11,14 @@ def main():
 def alice():
     winTracker = {'x':0, 'o':0}
     for i in range(50):
-        winTracker[requests.get("http://alice:5000/alice").text] += 1
+        winTracker[requests.get("http://game:5000/alice").text] += 1
     if (winTracker['x'] == winTracker['o']):
         return "Alice does equally well as both x and o!\n" + str(winTracker)
     return "Alice does better as " + max(winTracker, key=winTracker.get) + '.\n' + str(winTracker)
 def alicevbaab():
     winTracker = {'Alice':0, 'Baab':0}
-    for i in range(20):
-        winTracker[requests.get("http://alice:5000/alicevbaab").text] += 1
+    for i in range(50):
+        winTracker[requests.get("http://game:5000/alicevbaab").text] += 1
     if (winTracker['Alice'] == winTracker['Baab']):
         return "Alice and Baab are both great!\n" + str(winTracker)
     return max(winTracker, key=winTracker.get) + " does better.\n" + str(winTracker)
